b28daeb2027c155638b38b54aa8b379a7d021d04
edrx2@live.com <edrx2@live.com>
2023-03-01 21:16:41 +0000

    AB#1

--- petstore/petstoredataops/notebooks/newnotebook.py
+++ petstore/petstoredataops/notebooks/newnotebook.py
@@ -1,6 +1,6 @@
 # Databricks notebook source
 # MAGIC %sql
-# MAGIC SELECT * FROM `hive_metastore`.`default`.`breeds_1` where _c3 == 'Dog';
+# MAGIC SELECT * FROM `hive_metastore`.`default`.`breeds_1` where _c3 == 'Cat';
 
 # COMMAND ----------
 
